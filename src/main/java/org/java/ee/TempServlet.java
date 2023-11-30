829f5c7a85c8b7ea18456043db11bc461cb3b987
Yana Yasinvski <jantschick3@gmail.com>
2023-11-30 13:23:34 +0100

    return status

--- src/main/java/org/java/ee/TempServlet.java
+++ src/main/java/org/java/ee/TempServlet.java
@@ -16,10 +16,13 @@ public class TempServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if(req.getHeader("Accept-Encoding").contains("gzip")) {
-            PrintWriter printWriter = new PrintWriter(new GZIPOutputStream(resp.getOutputStream()));
-            printWriter.write("hello world");
-        }
+//        resp.setStatus(HttpServletResponse.SC_OK);
+//        resp.sendRedirect("/hello");
+//        resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "blalaa");
+
+//        resp.setHeader("Refresh", "1");
+//        System.out.println("hello");
+        resp.setHeader("Refresh", "5;URL=https://google.com");
     }
 
 }
