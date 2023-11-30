c9b8c7545a1e23ab13f6ed0321be673d3bdad902
Yana Yasinvski <jantschick3@gmail.com>
2023-11-30 13:15:53 +0100

    gzip

--- src/main/java/org/java/ee/TempServlet.java
+++ src/main/java/org/java/ee/TempServlet.java
@@ -6,33 +6,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Enumeration;
 import java.util.Map;
+import java.util.zip.GZIPOutputStream;
 
 @WebServlet("/temp")
 public class TempServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        Enumeration<String> headerNames = req.getHeaderNames();
-//        while (headerNames.hasMoreElements()) {
-//            String s = headerNames.nextElement();
-//            System.out.println(s + " = " + req.getHeader(s));
-//        }
-
-        System.out.println(req.getAuthType());
-        System.out.println(req.getContentType());
-        System.out.println(req.getContentLength());
-        System.out.println(req.getMethod());
-        System.out.println(req.getRequestURI());
-        System.out.println(req.getQueryString());
-        System.out.println(req.getProtocol());
-
-        resp.setHeader("Content-Length:", "100");
+        if(req.getHeader("Accept-Encoding").contains("gzip")) {
+            PrintWriter printWriter = new PrintWriter(new GZIPOutputStream(resp.getOutputStream()));
+            printWriter.write("hello world");
+        }
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
-    }
 }
