30cf3c4d9b2f9c5aa619f54b8d9ab7db7a368d36
Yana Yasinvski <jantschick3@gmail.com>
2023-11-30 11:45:06 +0100

    doGet

--- src/main/java/org/java/ee/ServletLiveCycle.java
+++ src/main/java/org/java/ee/ServletLiveCycle.java
@@ -1,2 +1,44 @@
-package org.java.ee;public class ServletLiveCycle  {
+package org.java.ee;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/LiveCycle")
+public class ServletLiveCycle extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("initialization");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String s = "<html>" +
+                "<header><title>This is title</title></header>" +
+                "<body>" +
+                "Hello world" +
+                "</html>";
+
+        resp.getWriter().write(s);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("service method");
+        super.service(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
 }
--- src/main/java/org/java/ee/TempServlet.java
+++ src/main/java/org/java/ee/TempServlet.java
@@ -1,2 +1,46 @@
-package org.java.ee;public class TempServlet  {
+package org.java.ee;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+
+@WebServlet("/temp")
+public class TempServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        String one = req.getParameter("one");
+//        String two = req.getParameter("two");
+//        System.out.println(one);
+//        String[] ones = req.getParameterValues("one");
+//        for (String s: ones) {
+//            System.out.println(s);
+//        }
+
+//        Enumeration<String> parameterNames = req.getParameterNames();
+//        while (parameterNames.hasMoreElements()) {
+//            String element = parameterNames.nextElement();
+//            System.out.println(element + " = " + req.getParameter(element));
+//        }
+//
+//        Map<String, String[]> parameterMap = req.getParameterMap();
+
+        System.out.println(req.getRequestURL());
+        System.out.println(req.getServletPath());
+        System.out.println(req.getRemoteHost());
+        System.out.println(req.getRequestURI());
+        System.out.println(req.getLocalPort());
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
